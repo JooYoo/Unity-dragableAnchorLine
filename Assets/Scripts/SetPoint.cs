ca58bfd3479c413e8609aaa679862dfe0e39067d
JooYoo <jooyoo@outlook.com>
2018-10-11 15:03:19 +0200

    basic function works

--- Assets/Scripts/SetPoint.cs
+++ Assets/Scripts/SetPoint.cs
@@ -1,4 +1,5 @@
-﻿using Boo.Lang;
+﻿using System;
+using Boo.Lang;
 using UnityEngine;
 
 public class SetPoint : MonoBehaviour
@@ -7,41 +8,41 @@ public class SetPoint : MonoBehaviour
     private List<GameObject> connectionLines = new List<GameObject>();
     public GameObject dragableCube;
 
-
-    private Vector3 dist;
-    private float posX;
-    private float posY;
-
     Ray ray;
     RaycastHit hit;
+    private int nameIndex = 0;
+    private int selectedObjIndex;
 
-    private bool isEnter = false;
 
-  
-
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(0)) // drag anchor 
         {
-            Debug.Log("clicked left btn!!!");
-            //get mouse position
-            dist = Camera.main.WorldToScreenPoint(transform.position);
-            posX = Input.mousePosition.x - dist.x;
-            posY = Input.mousePosition.y - dist.y;
+            Ray currentRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit currentHit;
+            GameObject hitMeasureCube;
+            GameObject findGameObj;
+            if (Physics.Raycast(currentRay, out currentHit))
+            {
+                hitMeasureCube = currentHit.collider.gameObject;
+                measureObjs.Find(x => x.name == hitMeasureCube.name, out findGameObj);
+                // get selected Obj Index
+                selectedObjIndex = Int32.Parse((findGameObj.name[findGameObj.name.Length - 1].ToString()));
+            }
 
+        }
+
+        if (Input.GetMouseButtonDown(1)) // create anchor cube and connection lines
+        {
+            //create measure cube
             ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hit))
             {
-                //create measure cube
-                GameObject obj = Instantiate(dragableCube, new Vector3(hit.point.x, hit.point.y, hit.point.z),Quaternion.identity);
+                GameObject obj = Instantiate(dragableCube, new Vector3(hit.point.x, hit.point.y, hit.point.z), Quaternion.identity);
+                obj.name = "cube" + nameIndex;
                 measureObjs.Add(obj);
+
+                nameIndex++;
             }
 
             // draw lines
@@ -49,29 +50,37 @@ public class SetPoint : MonoBehaviour
             {
                 var startPoint = measureObjs.Count - 2;
                 var endPoint = measureObjs.Count - 1;
-
                 DrawLine(measureObjs[startPoint].transform.position, measureObjs[endPoint].transform.position);
             }
         }
+      
 
-        //if (Input.GetKeyDown("return"))
-        //{
-        //    isEnter = true;
-        //    Debug.Log(isEnter);
-        //}
-
-        if (measureObjs.Count >1)
+        if (Input.GetMouseButton(0)) // change drag the 
         {
-            //connectionLines[0].transform.position = measureObjs[0].transform.position;
-            connectionLines[0].GetComponent<LineRenderer>().SetPosition(1, measureObjs[1].transform.position);
-        }
-    }
+            if (selectedObjIndex != 0) // the first measureCube without left line
+            {
+                // left Line: start                
+                connectionLines[selectedObjIndex - 1].GetComponent<LineRenderer>().SetPosition(0, measureObjs[selectedObjIndex - 1].transform.position);
+                // end
+                connectionLines[selectedObjIndex - 1].GetComponent<LineRenderer>().SetPosition(1, measureObjs[selectedObjIndex].transform.position);
+            }
 
+            if (selectedObjIndex != measureObjs.Count -1) // the last measureCube without left line
+            {
+                // right Line: start
+                connectionLines[selectedObjIndex].GetComponent<LineRenderer>().SetPosition(0, measureObjs[selectedObjIndex].transform.position);
+                // end
+                connectionLines[selectedObjIndex].GetComponent<LineRenderer>().SetPosition(1, measureObjs[selectedObjIndex + 1].transform.position);
+            }
+        }
+
+    }
 
 
     void DrawLine(Vector3 start, Vector3 end)
     {
         var myLine = new GameObject();
+        myLine.name = "line" + (nameIndex - 2);
         connectionLines.Add(myLine);
 
         myLine.transform.position = start;
